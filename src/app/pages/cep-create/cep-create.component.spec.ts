d0e6eda25c94a5c93e8cd1f98a4d8fe1b1ba885d
José Bruno <josebrunocampanholi@gmail.com>
2021-08-22 20:30:43 -0300

    Teste em componente CEP

--- src/app/pages/cep-create/cep-create.component.spec.ts
+++ src/app/pages/cep-create/cep-create.component.spec.ts
@@ -1,14 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CepServiceService } from 'src/core/services/cep-service.service';
 
 import { CepCreateComponent } from './cep-create.component';
 
-describe('CepCreateComponent', () => {
+
+describe('Testando pagina de criacao de CEP', () => {
   let component: CepCreateComponent;
   let fixture: ComponentFixture<CepCreateComponent>;
-
+  let injector:TestBed;
+  let cepService:CepServiceService;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CepCreateComponent ]
+      imports: [],
+      declarations: [ CepCreateComponent ],
+      providers: [
+          {provide: CepServiceService}
+      ]
     })
     .compileComponents();
   });
@@ -17,9 +26,13 @@ describe('CepCreateComponent', () => {
     fixture = TestBed.createComponent(CepCreateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    cepService = injector.inject(CepServiceService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('CEPS não podem ter números repetitivos em pares;', () => {
+    const form = fixture.nativeElement.querySelector('form');
+    let cep = '121359';
+    form.submit();
+    expect(cep).toBeNull();
   });
 });
